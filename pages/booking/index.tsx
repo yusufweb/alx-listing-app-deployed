a0daa2461efdf79e325f9f22ec2a539fec5ca6ca
yusufweb <yusuffajao4@gmail.com>
2025-06-29 21:33:19 +0100

    done

--- pages/booking/index.tsx
+++ pages/booking/index.tsx
@@ -1,7 +1,21 @@
-import React from "react";  
+import BookingForm from "@/components/booking/BookingForm";
+import OrderSummary from "@/components/booking/OrderSummary";
 
-const BookingPage = () => {
+export default function BookingPage() {
+  const bookingDetails = {
+    propertyName: "Villa Arrecife Beach House",
+    price: 7500,
+    bookingFee: 65,
+    totalNights: 3,
+    startDate: "24 August 2024",
+  };
 
+  return (
+    <div className="container mx-auto p-6">
+      <div className="grid grid-cols-2 gap-6">
+        <BookingForm />
+        <OrderSummary bookingDetails={bookingDetails} />
+      </div>
+    </div>
+  );
 }
-
-export default BookingPage
