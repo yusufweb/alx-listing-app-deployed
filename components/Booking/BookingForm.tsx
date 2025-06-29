2d00e70df3f8992286a09a73d3e248ff5e9e72bc
yusufweb <yusuffajao4@gmail.com>
2025-06-29 21:23:47 +0100

    done

---
+++ components/Booking/BookingForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";  
+
+const BookingForm = () => {
+
+}
+
+export default BookingForm
---
+++ pages/booking/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";  
+
+const BookingPage = () => {
+
+}
+
+export default BookingPage
