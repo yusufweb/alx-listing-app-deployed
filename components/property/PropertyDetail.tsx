328fa63269b8acad37cd906b6313bb13f6d27ee4
yusufweb <yusuffajao4@gmail.com>
2025-07-26 20:19:17 +0100

    save and share button fixed

--- components/property/PropertyDetail.tsx
+++ components/property/PropertyDetail.tsx
@@ -12,44 +12,45 @@ import { useRouter } from "next/router";
 const PropertyDetail: React.FC<{ property: PropertyProps }> = ({
   property,
 }) => {
-
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div className="lg:p-10 md:p-10 lg:pt-2 md:pt-2 relative">
       <div className="flex flex-col">
         <div className="flex justify-between items-center px-6 pt-6 lg:hidden md:hidden">
-          <Link href="/">
-            <div className="flex justify-between items-center gap-2">
-              <Image
-                src="/assets/icons/Linear/Arrows/Arrow Left.png"
-                alt="star Image"
-                width={20}
-                height={20}
-              />
-              <p className="font-semibold text-gray-600">Return</p>
-            </div>
-          </Link>
-          <div className="flex justify-between items-center gap-2">
-            <Button size="medium" shape="rounded-full" className="border-0">
+          <div className="w-auto">
+            <Link href="/">
+              <div className="flex justify-between items-center gap-2">
+                <Image
+                  src="/assets/icons/Linear/Arrows/Arrow Left.png"
+                  alt="star Image"
+                  width={25}
+                  height={25}
+                />
+                <p className="font-semibold text-gray-600">Return</p>
+              </div>
+            </Link>
+          </div>
+          <div className="flex justify-between items-center space-x-14">
+            {/* <Button size="medium" shape="rounded-full" className="border-0"> */}
               <div className="flex justify-center items-center gap-2">
                 <Image
                   src="/assets/icons/Fav.png"
-                  width={20}
-                  height={20}
+                  width={25}
+                  height={25}
                   alt="Fav"
                 />
               </div>
-            </Button>
-            <Button size="medium" shape="rounded-full" className="border-0">
+            {/* </Button> */}
+            {/* <Button size="medium" shape="rounded-full" className="border-0"> */}
               <div className="flex justify-center items-center gap-2">
                 <Image
                   src="/assets/icons/Share.png"
-                  width={20}
-                  height={20}
+                  width={25}
+                  height={25}
                   alt="save"
                 />
               </div>
-            </Button>
+            {/* </Button> */}
           </div>
         </div>
         <div className="flex justify-between items-center order-2 mt-6 lg:order-0 md:order-0 px-6 md:px-0 lg:px-0">
@@ -166,7 +167,9 @@ const PropertyDetail: React.FC<{ property: PropertyProps }> = ({
               height={15}
               alt="filter"
             />
-            <p className="text-[10px] lg:text-[15px] md:text-[14px]">{property.offers.bed} Bedroom</p>
+            <p className="text-[10px] lg:text-[15px] md:text-[14px]">
+              {property.offers.bed} Bedroom
+            </p>
           </div>
         </Button>
         <Button size="small" shape="rounded-full">
@@ -177,7 +180,9 @@ const PropertyDetail: React.FC<{ property: PropertyProps }> = ({
               height={15}
               alt="filter"
             />
-            <p className="text-[10px] lg:text-[15px] md:text-[14px]">{property.offers.shower} Bathroom</p>
+            <p className="text-[10px] lg:text-[15px] md:text-[14px]">
+              {property.offers.shower} Bathroom
+            </p>
           </div>
         </Button>
         <Button size="small" shape="rounded-full">
@@ -188,7 +193,9 @@ const PropertyDetail: React.FC<{ property: PropertyProps }> = ({
               height={15}
               alt="filter"
             />
-            <p className="text-[10px] lg:text-[15px] md:text-[14px]">{property.offers.occupants} Guest</p>
+            <p className="text-[10px] lg:text-[15px] md:text-[14px]">
+              {property.offers.occupants} Guest
+            </p>
           </div>
         </Button>
       </div>
@@ -306,19 +313,29 @@ const PropertyDetail: React.FC<{ property: PropertyProps }> = ({
           <ReviewSection reviews={REVIEWS} />
         </div>
         <div className="lg:w-1/3 md:w-1/2 hidden lg:block md:block">
-          <BookingSection
-            property= {property}
-          />
+          <BookingSection property={property} />
         </div>
       </div>
 
-    {/* display on Mobile screen */}
+      {/* display on Mobile screen */}
       <div className="flex justify-between items-center p-4 border-t-1 border-t-gray-100 fixed bottom-0 left-0 w-full bg-white md:hidden lg:hidden">
         <div className="flex flex-col space-y-2 w-1/3">
-            <p className="text-dark font-bold">${property.price}/ <span className="text-gray-400 font-medium text-[12px]">nights</span></p>
-            <input type="text"  placeholder="Add date"/>
+          <p className="text-dark font-bold">
+            ${property.price}/{" "}
+            <span className="text-gray-400 font-medium text-[12px]">
+              nights
+            </span>
+          </p>
+          <input type="text" placeholder="Add date" />
         </div>
-        <button onClick={() => {router.push("/booking")}} className="bg-emerald-600 py-4 px-8 rounded-lg text-white font-semibold w-1/2 text-[14px]">Review now</button>
+        <button
+          onClick={() => {
+            router.push("/booking");
+          }}
+          className="bg-emerald-600 py-4 px-8 rounded-lg text-white font-semibold w-1/2 text-[14px]"
+        >
+          Review now
+        </button>
       </div>
     </div>
   );
